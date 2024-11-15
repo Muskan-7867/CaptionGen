f03927eca78605219d838e5b17a88b04bb829a6b
muskan <muskanloach984@gmail.com>
2024-11-15 16:51:24 +0530

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 // src/App.tsx
 import React from 'react';
 import ContentGenerator from './components/ContentGen';
+import Navbar from './components/Navbar';
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Navbar />
       <ContentGenerator />
     </div>
   );
